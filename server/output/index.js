29b617dfedfe4e6cabdcc9093f8dde6553522fa9
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-03-01 10:04:30 -0500

    configuring database connection

--- server/output/index.js
+++ server/output/index.js
@@ -4,22 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const mongoose = require("mongoose");
-const cors = require("cors");
 if (process.env.NODE_ENV === "development")
     require("../secrets.js");
-const dbUri = process.env.DBURI;
+const dbUri = process.env.URI;
 const clientHost = process.env.NODE_ENV === "development" ?
     "http://localhost:4200" : process.env.CLIENT_HOST;
 // common middleware
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use(cors({ origin: clientHost }));
+app.use((0, cors_1.default)({ origin: clientHost })); // this enables requests from client
 // landing page
 app.get("/", (req, res, next) => {
     res.json({ name: "Expedia clone api" });
 });
-app.listen(8080, async () => {
-    console.log("Server listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, async () => {
+    try {
+        // @ts-ignore
+        await mongoose_1.default.connect(dbUri);
+        console.log("DB Connected");
+        console.log(`Server listening on port ${port}`);
+    }
+    catch (err) {
+        console.error("DB Connection error", err);
+    }
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,31 @@
 import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
 
 if (process.env.NODE_ENV === "development") require("../secrets.js");
-const dbUri: string | undefined = process.env.DBURI;
-const clientHost: string | undefined = process.env.NODE_ENV === "development" ? 
+const dbUri = process.env.URI;
+const clientHost = process.env.NODE_ENV === "development" ? 
     "http://localhost:4200" : process.env.CLIENT_HOST;
 
 // common middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: clientHost }));
+app.use(cors({ origin: clientHost })); // this enables requests from client
 
 // landing page
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.json({ name: "Expedia clone api" });
 })
 
-
-app.listen(8080, async () => {
-    console.log("Server listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, async () => {
+    try {
+        // @ts-ignore
+        await mongoose.connect(dbUri);
+        console.log("DB Connected");
+        console.log(`Server listening on port ${port}`);
+    } catch(err) {
+        console.error("DB Connection error", err);
+    }
 })
