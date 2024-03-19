26a40322bc2be5dcb80002e2f8a8e3195e884d6e
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-03-19 15:55:56 -0400

    method for handling date range in search

--- server/src/controllers/hotels.ts
+++ server/src/controllers/hotels.ts
@@ -1,19 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import { HotelModel } from "../models";
 
+
 class HotelController {
+    private readonly MILLISECONDS_DAY = 8.64 * 10^7;
+    /*
+        purpose: fill in dates in between the from and to dates provided by search
+        params: fromDate and toDate from user
+        returns: array of dates in chronoligical order
 
+    */
     private handleSearchDates(fromDate: Date, toDate: Date): Date[] {
+        // i.e., if there are no dates in between fromDate and toDate
+        if ((fromDate.valueOf() + this.MILLISECONDS_DAY) === toDate.valueOf()) return [fromDate, toDate];
+        const dateRange: Date[] = []
 
-        console.dir(fromDate);
-        console.dir(toDate);
-        return [fromDate, toDate] // need to put dates in between
+        // initializing nextDate as one day after fromDate
+        let nextDate = new Date(fromDate.valueOf() + this.MILLISECONDS_DAY);
+
+        while (nextDate.valueOf() !== toDate.valueOf()) {
+            dateRange.push(nextDate);
+            // increment by one day
+            nextDate = new Date(nextDate.valueOf() + this.MILLISECONDS_DAY);
+        }
+        return dateRange;
     }
-
+    /*
+        purpose: route handler for hotel searching: response with list of hotels that match criteria
+        params: request object, response object, and next function for calling the error middleware
+        returns: void
+    */
     getHotelsFromSearch = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const fromDate = req.query.fromDate ? new Date(req.query.fromDate.toString()) : new Date(Date.now());
-            const toDate = req.query.toDate ? new Date(req.query.toDate.toString()) : new Date(Date.now() + (2.592 * 10 ^ 8));
+            const toDate = req.query.toDate ? new Date(req.query.toDate.toString()) : new Date(Date.now() + this.MILLISECONDS_DAY * 3);
             const dates = this.handleSearchDates(fromDate, toDate);
             const searchParams = { location: req.query.location, guests: Number(req.query.guests), dates };
             throw new Error("Stop!!!");
@@ -34,7 +54,12 @@ class HotelController {
         }
     }
 
-    async getHotelToShow(req: Request, res: Response, next: NextFunction) {
+    /*
+        purpose: route handler to show single hotel; response with hotel that matches given id
+        params: request object, response object, and next function for calling the error middleware
+        returns: void
+    */
+    getHotelToShow = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const date = req.query.dates && new Date(req.query.dates.toString());
             const searchParams = { guests: Number(req.query.guests), dates: date };
